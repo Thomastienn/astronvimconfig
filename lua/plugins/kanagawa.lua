e083091beb6b51b9289b1d4c594a25aa6541c6b0
Thomastienn <thomastien123@gmail.com>
2025-06-09 13:57:59 -0600

    add

--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -2,9 +2,4 @@ return {
   "thesimonho/kanagawa-paper.nvim",
   lazy = false,
   priority = 1000,
-  opts = {},
-  config = function()
-    vim.o.background = "light"
-    vim.cmd.colorscheme "kanagawa-paper-canvas"
-  end,
 }
