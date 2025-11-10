227401766e2509e9ce91ec76deb0fdb2995cfee6
Thomastienn <thomastien123@gmail.com>
2025-11-10 13:17:32 -0700

    use dark mode

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -2,6 +2,6 @@ return {
   "AstroNvim/astroui",
   ---@type AstroUIOpts
   opts = {
-    colorscheme = "kanagawa-paper-canvas",
+    colorscheme = "kanagawa-paper-ink",
   },
 }
--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -2,4 +2,8 @@ return {
   "thesimonho/kanagawa-paper.nvim",
   lazy = false,
   priority = 1000,
+  init = function()
+    vim.o.background = 'dark'
+    vim.cmd.colorscheme("kanagawa-paper-ink")
+  end,
 }
