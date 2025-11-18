3299497cff8bfc33d85f7fbe096cdefde4480716
Thomastienn <thomastien123@gmail.com>
2025-11-18 16:49:34 -0700

    move into file

--- lua/plugins/leetcode.lua
+++ lua/plugins/leetcode.lua
@@ -10,4 +10,13 @@ return {
         -- configuration goes here
         lang="python3",
     },
+    keys = {
+        { "<leader>lcc", "<cmd>Leet<CR>", desc = "Toggle leetcode" },
+        { "<leader>lcr", "<cmd>Leet run<CR>", desc = "Run" },
+        { "<leader>lcs", "<cmd>Leet submit<CR>", desc = "Submit" },
+        { "<leader>lcl", "<cmd>Leet lang<CR>", desc = "Pick language" },
+        { "<leader>lci", "<cmd>Leet info<CR>", desc = "Info" },
+        { "<leader>lcd", "<cmd>Leet desc<CR>", desc = "Description tab" },
+        { "<leader>lcy", "<cmd>Leet yank<CR>", desc = "Yank" },
+    }
 }
