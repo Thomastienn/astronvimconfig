853b736fcbd6c9748b19f17192c94c228318aad8
Thomastienn <thomastien123@gmail.com>
2025-11-20 12:47:51 -0700

    fix full path

--- g_engine_run.sh
+++ g_engine_run.sh
@@ -1,2 +1,10 @@
-#!/bin/bash
-tmux new-window "nvim \"$1\""
+#!/usr/bin/env bash
+# g_engine_run.sh - open file in kitty running nvim
+
+# use full paths to avoid PATH issues when Godot runs it from desktop
+KITTY=/home/thomas/.local/kitty.app/bin/kitty
+NVIM=/home/linuxbrew/.linuxbrew/bin/nvim
+
+# $1 is the file path passed by Godot
+exec "$KITTY" "$NVIM" "$1"
+
