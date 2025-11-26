ad3264d0b7a9078464a42f48e36456a29c5a5de9
Thomastienn <thomastien123@gmail.com>
2025-11-26 12:26:04 -0700

    mst update

---
+++ snippets/python/kruskal.py
@@ -0,0 +1,18 @@
+def kruskal_mst(n, edges):
+    dsu = DSU(n) # type: ignore # noqa
+    
+    # Sort edges by weight
+    edges.sort(key=lambda x: x[2])
+    
+    total_weight = 0
+    mst_edges = []
+    
+    for u, v, weight in edges:
+        if dsu.find(u) != dsu.find(v):
+            dsu.union(u, v)
+            total_weight += weight
+            mst_edges.append((u, v, weight))
+            if len(mst_edges) == n - 1:
+                break
+    
+    return total_weight, mst_edges
---
+++ snippets/python/prim.py
@@ -0,0 +1,25 @@
+def prim_mst(n, adj):
+    import heapq
+    
+    visited = [False] * n
+    min_heap = [(0, 0, -1)]  # (weight, node, parent)
+    total_weight = 0
+    edges = []
+    
+    while min_heap:
+        weight, u, parent = heapq.heappop(min_heap)
+        
+        if visited[u]:
+            continue
+        
+        visited[u] = True
+        total_weight += weight
+        
+        if parent != -1:
+            edges.append((parent, u, weight))
+        
+        for v, w in adj[u]:
+            if not visited[v]:
+                heapq.heappush(min_heap, (w, v, u))
+    
+    return total_weight, edges
