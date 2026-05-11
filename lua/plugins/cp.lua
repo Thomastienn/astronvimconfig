99beb783c0068a73676713981a7d45468229dab0
Thomastienn <thomastien123@gmail.com>
2026-05-11 17:23:46 -0600

    a way to change lang

--- lua/plugins/cp.lua
+++ lua/plugins/cp.lua
@@ -1,117 +1,224 @@
---@diagnostic disable
+-- @diagnostic disable
+
+local lang = "py"
+
+local function competitest_config()
+	return {
+		runner_ui = {
+			interface = "popup", -- "popup" | "split"
+		},
+
+		received_files_extension = lang,
+
+		template_file = {
+			cpp = "~/.config/nvim/snippets/cpp/cp.cpp",
+			py = "~/.config/nvim/snippets/python/cp.py",
+			rs = "~/.config/nvim/snippets/rust/cp.rs",
+		},
+
+		received_problems_path = "$(CWD)/$(JAVA_TASK_CLASS)/$(JAVA_TASK_CLASS).$(FEXT)",
+		received_contests_directory = "$(CWD)/$(CONTEST)",
+
+		compile_command = {
+			cpp = {
+				exec = "g++",
+				args = {
+					"-O2",
+					"-std=c++20",
+					"-DLOCAL",
+					"-Wall",
+					"-Wextra",
+					"-Wshadow",
+					"-fsanitize=undefined",
+					"$(FNAME)",
+					"-o",
+					"$(FNOEXT)",
+				},
+			},
+		},
+
+		testcases_use_single_file = true,
+		view_output_diff = true,
+		evaluate_template_modifiers = true,
+	}
+end
+
+local function setup_competitest()
+	require("competitest").setup(competitest_config())
+end
+
+local function disable_help()
+	local group = vim.api.nvim_create_augroup("CompetiTestDisableHelp", {
+		clear = true,
+	})
+
+	vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+		group = group,
+		pattern = "*.cpp",
+		callback = function(args)
+			if vim.lsp.inlay_hint then
+				vim.lsp.inlay_hint.enable(false, {
+					bufnr = args.buf,
+				})
+			end
+
+			vim.cmd("silent! Copilot disable")
+		end,
+	})
+end
 
 return {
-	'xeluxee/competitest.nvim',
-	dependencies = 'MunifTanjim/nui.nvim',
-	config = function()
-	    require('competitest').setup({
-	        runner_ui = {
-	            interface = "popup" -- "popup" | "split"
-	        },
-	        received_files_extension = "rs",
-	        template_file = {
-	        	cpp = "~/.config/nvim/snippets/cpp/cp.cpp",
- 				py = "~/.config/nvim/snippets/python/cp.py",
- 				rs = "~/.config/nvim/snippets/rust/cp.rs",
-	        },
-	        received_problems_path = "$(CWD)/$(JAVA_TASK_CLASS)/$(JAVA_TASK_CLASS).$(FEXT)",
-	        received_contests_directory = "$(CWD)/$(CONTEST)",
-	        compile_command = {
-	        	cpp = { exec = "g++", args = { "-O2", "-std=c++20", "-DLOCAL", "-Wall", "-Wextra", "-Wshadow", "-fsanitize=undefined", "$(FNAME)", "-o", "$(FNOEXT)" } },
-	        },
-	        testcases_use_single_file = true,
-	        view_output_diff = true,
-	        evaluate_template_modifiers = true,
-	    })
-	    local function disable_help()
-			vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
-				pattern = "*.cpp",
-				callback = function()
-					-- Disable inlay hints
-					vim.lsp.inlay_hint.enable(false, { bufnr = 0 })
+	"xeluxee/competitest.nvim",
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+	},
 
-					-- Disable GitHub Copilot
-					vim.cmd("Copilot disable")
+	config = function()
+		setup_competitest()
+
+		vim.keymap.set("n", "<leader>rtl", function()
+			vim.ui.select({
+				{ name = "Rust", ext = "rs" },
+				{ name = "C++", ext = "cpp" },
+				{ name = "Python", ext = "py" },
+			}, {
+				prompt = "Choose CompetiTest language:",
+				format_item = function(item)
+					return item.name .. " (." .. item.ext .. ")"
 				end,
-			})
-	    end
-		-- Control processes
+			}, function(choice)
+				if not choice then
+					return
+				end
+
+				lang = choice.ext
+				setup_competitest()
+
+				vim.notify(
+					"CompetiTest language set to ." .. lang,
+					vim.log.levels.INFO
+				)
+			end)
+		end, {
+			desc = "Change CompetiTest language",
+		})
+
+		-- Control processes:
 		-- Run again a testcase by pressing R
 		-- Run again all testcases by pressing <C-r>
 		-- Kill the process associated with a testcase by pressing K
-		-- Kill all the processes associated with testcases by pressing <C-k>
-		vim.keymap.set("n", "<leader>rtt", ":CompetiTest run<CR>", { desc = "Toggle/Run competitest" })
-		vim.keymap.set("n", "<leader>rtu", ":CompetiTest show_ui<CR>", { desc = "Open UI" })
-		vim.keymap.set("n", "<leader>rtd", ":CompetiTest delete_testcase<CR>", { desc = "Delete testcase" })
-		vim.keymap.set("n", "<leader>rtc", ":CompetiTest run_no_compile<CR>", { desc = "Run no compile" })
-		vim.keymap.set("n", "<leader>rta", ":CompetiTest add_testcase<CR>", { desc = "Add testcase" })
-		vim.keymap.set("n", "<leader>rte", ":CompetiTest edit_testcase<CR>", { desc = "Edit testcase" })
+		-- Kill all testcase processes by pressing <C-k>
+
+		vim.keymap.set("n", "<leader>rtt", "<cmd>CompetiTest run<CR>", {
+			desc = "Toggle/Run CompetiTest",
+		})
+
+		vim.keymap.set("n", "<leader>rtu", "<cmd>CompetiTest show_ui<CR>", {
+			desc = "Open CompetiTest UI",
+		})
+
+		vim.keymap.set("n", "<leader>rtd", "<cmd>CompetiTest delete_testcase<CR>", {
+			desc = "Delete testcase",
+		})
+
+		vim.keymap.set("n", "<leader>rtc", "<cmd>CompetiTest run_no_compile<CR>", {
+			desc = "Run no compile",
+		})
+
+		vim.keymap.set("n", "<leader>rta", "<cmd>CompetiTest add_testcase<CR>", {
+			desc = "Add testcase",
+		})
+
+		vim.keymap.set("n", "<leader>rte", "<cmd>CompetiTest edit_testcase<CR>", {
+			desc = "Edit testcase",
+		})
+
 		vim.keymap.set("n", "<leader>rtp", function()
-			-- ":CompetiTest receive problem<CR>"
 			vim.cmd("CompetiTest receive problem")
 			disable_help()
-		end, { desc = "Receive problem" })
+		end, {
+			desc = "Receive problem",
+		})
+
 		vim.keymap.set("n", "<leader>rtn", function()
-			-- ":CompetiTest receive contest<CR>"
 			vim.cmd("CompetiTest receive contest")
 			disable_help()
-		end, { desc = "Receive contest" })
+		end, {
+			desc = "Receive contest",
+		})
 
 		vim.keymap.set("n", "<leader>rtb", function()
-    		local filepath = vim.fn.expand("%:p:h")
-    		local filename = vim.fn.expand("%:t:r")
-    		local extension = vim.fn.expand("%:e")
-    		local main_file = vim.fn.expand("%:p")
-    		local brute_filename = filepath .. "/" .. filename .. "_brute." .. extension
-    		local gen_filename = filepath .. "/" .. filename .. "_gen.py"
-    		local bash_filename = filepath .. "/diff_" .. filename .. ".sh"
+			local filepath = vim.fn.expand("%:p:h")
+			local filename = vim.fn.expand("%:t:r")
+			local extension = vim.fn.expand("%:e")
+			local main_file = vim.fn.expand("%:p")
 
-    		local template_dir = vim.fn.expand("~/.config/nvim/snippets/")
-    		local bash_template = template_dir .. "bash/diff.sh"
-    		local gen_template = template_dir .. "python/brute/gen.py"
+			local brute_filename = filepath .. "/" .. filename .. "_brute." .. extension
+			local gen_filename = filepath .. "/" .. filename .. "_gen.py"
+			local bash_filename = filepath .. "/diff_" .. filename .. ".sh"
 
-    		local function read_file(path)
-        		local f = io.open(path, "r")
-        		if not f then return nil end
-        		local content = f:read("*a")
-        		f:close()
-        		return content
-    		end
+			local template_dir = vim.fn.expand("~/.config/nvim/snippets/")
+			local bash_template = template_dir .. "bash/diff.sh"
+			local gen_template = template_dir .. "python/brute/gen.py"
 
-    		local function write_file(path, content)
-        		local f = io.open(path, "w")
-        		if not f then return false end
-        		f:write(content)
-        		f:close()
-        		return true
-    		end
+			local function read_file(path)
+				local f = io.open(path, "r")
+				if not f then
+					return nil
+				end
 
-    		-- Copy main file to brute file (same extension)
-    		os.execute("cp " .. vim.fn.shellescape(main_file) .. " " .. vim.fn.shellescape(brute_filename))
+				local content = f:read("*a")
+				f:close()
 
-    		-- Use gen template
-    		local gen_content = read_file(gen_template)
-    		if gen_content then
-        		write_file(gen_filename, gen_content)
-    		else
-        		write_file(gen_filename, "")
-    		end
+				return content
+			end
 
-    		local bash_content = read_file(bash_template)
-    		if not bash_content then
-        		vim.notify("Error: missing " .. bash_template, vim.log.levels.ERROR)
-        		return
-    		end
+			local function write_file(path, content)
+				local f = io.open(path, "w")
+				if not f then
+					return false
+				end
 
-    		bash_content = bash_content:gsub("{{DIR}}", filepath)
-    		bash_content = bash_content:gsub("{{MAIN}}", main_file)
-    		bash_content = bash_content:gsub("{{BRUTE}}", brute_filename)
-    		bash_content = bash_content:gsub("{{GEN}}", gen_filename)
+				f:write(content)
+				f:close()
 
-    		write_file(bash_filename, bash_content)
-    		os.execute("chmod +x " .. bash_filename)
+				return true
+			end
 
-    		vim.notify("Created: " .. brute_filename .. ", " .. gen_filename .. ", " .. bash_filename, vim.log.levels.INFO)
-		end, { desc = "Brute force and diff" })
+			os.execute("cp " .. vim.fn.shellescape(main_file) .. " " .. vim.fn.shellescape(brute_filename))
+
+			local gen_content = read_file(gen_template)
+			if gen_content then
+				write_file(gen_filename, gen_content)
+			else
+				write_file(gen_filename, "")
+			end
+
+			local bash_content = read_file(bash_template)
+			if not bash_content then
+				vim.notify("Error: missing " .. bash_template, vim.log.levels.ERROR)
+				return
+			end
+
+			bash_content = bash_content:gsub("{{DIR}}", filepath)
+			bash_content = bash_content:gsub("{{MAIN}}", main_file)
+			bash_content = bash_content:gsub("{{BRUTE}}", brute_filename)
+			bash_content = bash_content:gsub("{{GEN}}", gen_filename)
+
+			write_file(bash_filename, bash_content)
+			os.execute("chmod +x " .. vim.fn.shellescape(bash_filename))
+
+			vim.notify(
+				"Created: "
+					.. brute_filename
+					.. ", "
+					.. gen_filename
+					.. ", "
+					.. bash_filename,
+				vim.log.levels.INFO
+			)
+		end, {
+			desc = "Brute force and diff",
+		})
 	end,
 }
